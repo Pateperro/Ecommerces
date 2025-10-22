2af02dd75f5fbb512bae3efe6a4159888e782e39
Miguel Angel Vargas <miguelangelvargasestevez@gmail.com>
2025-10-22 00:23:51 -0500

    mis vistas personalizadas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,26 +1,6 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class ProductController extends Controller
-{
-
-    // en los controladores es donde va la logica para no enfrascar el codigo y no generar sobrecargos
 
 
-    function index()
-    {
-            return view('products.index');
-    }
-
-    function create()
-    {
-            return view('products.create');
-    }
-
-    function show($id, $category = null)
+    function show()
     {  
 
         return view('products.show');

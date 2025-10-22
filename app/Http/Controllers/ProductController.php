9dd1a6c4fd9ae06ea56327e3d6f0a39fcbbba2ab
Miguel Angel Vargas <miguelangelvargasestevez@gmail.com>
2025-10-22 00:31:04 -0500

    mis vistas personalizadas --arreglo del error del productcotroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    // en los controladores es donde va la logica para no enfrascar el codigo y no generar sobrecargos
 
 
-    function show()
+    function index()
+    {
+            return view('products.index');
+    }
+
+    function create()
+    {
+            return view('products.create');
+    }
+
+    function show($id, $category = null)
     {  
 
         return view('products.show');
